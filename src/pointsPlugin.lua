9b7997989549c16711b6f27f6cbdbeaab5814443
jamesuni69 <125030847+jamesuni69@users.noreply.github.com>
2023-12-01 08:34:11 +0000

    Working On Assetto Hub

--- src/pointsPlugin.lua
+++ src/pointsPlugin.lua
@@ -35,7 +35,7 @@ local dangerouslySlowTimer = 0
 local carsState = {}
 local wheelsWarningTimeout = 0
 local ownSessionId = ac.getCar(0).sessionID
-local leaderboardUrl = "http://" .. ac.getServerIP() .. ":" .. ac.getServerPortHTTP() .. "/overtake/leaderboard"
+local leaderboardUrl = "http://" .. ac.getServerIP() .. ":" .. ac.getServerPortHTTP() .. "/overtake?leaderboard=SRP"
 
 function GetLeaderboard(callback)
     web.get(leaderboardUrl, function (err, response)
@@ -117,6 +117,126 @@ local raceUpdateEvent = ac.OnlineEvent({
     ac.debug("rivalHealth", rivalHealth)
     ac.debug("rivalRate", rivalRate)
 end)
+
+
+
+function GetSessionTime()
+    return ac.getSim().timeToSessionStart * -1
+end
+
+-- sending a new message:
+--raceUpdateEvent{ ownHealth = 0.6, ownRate = -0.2, rivalHealth = 0.3, rivalRate = -0.5 }
+--raceStatusEvent{ eventType = 1, eventData = 2 }
+
+local lastUiUpdate = GetSessionTime()
+function script.drawUI()
+
+    ac.debug("lastEvent", lastEvent)
+    ac.debug("rivalId", rivalId)
+
+    local currentTime = GetSessionTime()
+    local dt = currentTime - lastUiUpdate
+    lastUiUpdate = currentTime
+    local raceTimeElapsed = currentTime - raceStartTime
+
+    ac.debug("raceTimeElapsed", raceTimeElapsed)
+    ac.debug("dt", dt)
+
+    if lastEvent == EventType.RaceCountdown then
+        if raceTimeElapsed > -3000 and raceTimeElapsed < 0 then
+            DrawHealthHud(0)
+            local text = math.ceil(raceTimeElapsed / 1000 * -1)
+            DrawTextCentered(text)
+        elseif raceTimeElapsed > 0 then
+            if raceTimeElapsed < 1000 then
+                DrawTextCentered("Go!")
+            end
+
+            ownHealth = ownHealth + ownRate * (dt / 1000)
+            rivalHealth = rivalHealth + rivalRate * (dt / 1000)
+
+            DrawHealthHud(raceTimeElapsed)
+        end
+    end
+
+    if lastEvent == EventType.RaceEnded and raceEndTime > currentTime - 3000 then
+        DrawHealthHud(raceEndTime - raceStartTime)
+
+        if lastWinner == 255 then
+            DrawTextCentered("Race cancelled")
+        elseif lastWinner == ownSessionId then
+            DrawTextCentered("You won the race!")
+        else
+            DrawTextCentered("You lost the race.")
+        end
+    end
+end
+
+
+function DrawTextCentered(text)
+    local uiState = ac.getUI()
+
+    ui.transparentWindow('raceText', vec2(uiState.windowSize.x / 2 - 250, uiState.windowSize.y / 2 - 250), vec2(500,100), function ()
+        ui.pushFont(ui.Font.Huge)
+
+        local size = ui.measureText(text)
+        ui.setCursorX(ui.getCursorX() + ui.availableSpaceX() / 2 - (size.x / 2))
+        ui.text(text)
+
+        ui.popFont()
+    end)
+end
+
+local barColor = rgbm(1,1,1,1)
+function HealthBar(size, progress, direction)
+    progress = math.clamp(progress, 0, 1)
+
+    barColor:setLerp(rgbm.colors.red, rgbm.colors.white, progress)
+    ui.drawRect(ui.getCursor(), ui.getCursor() + size, barColor)
+
+    local p1, p2
+    if direction == -1 then
+        p1 = ui.getCursor() + vec2(size.x * (1 - progress), 0)
+        p2 = ui.getCursor() + size
+    else
+        p1 = ui.getCursor()
+        p2 = ui.getCursor() + vec2(size.x * progress, size.y)
+    end
+
+    ui.drawRectFilled(p1, p2, barColor)
+
+    ui.dummy(size)
+end
+
+function DrawHealthHud(time)
+    local uiState = ac.getUI()
+
+    ui.toolWindow('raceChallengeHUD', vec2(uiState.windowSize.x / 2 - 500, 25), vec2(1000, 120), function ()
+        ui.pushFont(ui.Font.Title)
+
+        ui.columns(3)
+        ui.text("PLAYER")
+        ui.nextColumn()
+
+        local laptime = ac.lapTimeToString(time)
+        local size = ui.measureText(laptime)
+
+        ui.setCursorX(ui.getCursorX() + ui.availableSpaceX() / 2 - (size.x / 2))
+        ui.text(laptime)
+        ui.nextColumn()
+        ui.textAligned("RIVAL", ui.Alignment.End, vec2(-1,0))
+
+        ui.columns(2)
+        HealthBar(vec2(ui.availableSpaceX(), 30), ownHealth, -1)
+        ui.textAligned(ac.getDriverName(0), ui.Alignment.Start, vec2(-1,0))
+        ui.nextColumn()
+        HealthBar(vec2(ui.availableSpaceX(), 30), rivalHealth, 1)
+        ui.textAligned(rivalName, ui.Alignment.End, vec2(-1,0))
+
+        ui.popFont()
+    end)
+end
+
 
 local image_0 = {
     ['src'] = 'https://i.imgur.com/QttPuh0.png',
@@ -235,6 +355,9 @@ function script.update(dt)
             state.nearMiss = false
         end
     end
+
+
+
 end
 
 -- For various reasons, this is the most questionable part, some UI. I don’t really like
